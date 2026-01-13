28a0c11b4d226718167dbf8e0abc39afe185537d
Akira Hayakawa <ruby.wktk@gmail.com>
2026-01-13 15:55:45 +0900

    Add benchmark

---
+++ benchmark/src/main.rs
@@ -0,0 +1,57 @@
+use clap::Parser;
+use linhash::*;
+use rand::Rng;
+use std::collections::HashSet;
+
+#[derive(Parser, Debug)]
+struct CommandArgs {
+    #[arg(long, default_value_t = 32)]
+    datasize: u32,
+    #[arg(long)]
+    warmup: u64,
+}
+
+fn main() {
+    let args = CommandArgs::parse();
+    dbg!(&args);
+
+    let dir = tempfile::tempdir().unwrap();
+    let mut db = LinHash::open(dir.path()).unwrap();
+
+    let mut keys = HashSet::new();
+
+    for _ in 0..args.warmup {
+        let key = random(32); // 256 bits key
+        let data = random(args.datasize as usize);
+        db.insert(key.clone(), data).unwrap();
+        keys.insert(key);
+    }
+
+    eprintln!("Warmup done. Starting benchmark...");
+
+    let mut results = vec![];
+
+    let t = std::time::Instant::now();
+    while t.elapsed() < std::time::Duration::from_secs(10) {
+        let timer = std::time::Instant::now();
+        for k in &keys {
+            let _ = db.get(k).unwrap();
+        }
+        let elapsed = timer.elapsed();
+        let latency = elapsed / (keys.len() as u32);
+        results.push(latency);
+    }
+
+    let mut sum = std::time::Duration::ZERO;
+    let n = results.len();
+    for r in results {
+        sum += r;
+    }
+
+    eprintln!("Latency: {:?}", sum / n as u32);
+}
+
+fn random(size: usize) -> Vec<u8> {
+    let mut rng = rand::rng();
+    (0..size).map(|_| rng.random()).collect()
+}
